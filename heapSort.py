c02ec88a2a4e428966be3167fde0fcac2d842f20
gabrielademoraes <gabrielademoraes10@gmail.com>
2019-05-10 17:08:48 -0300

    Adicionando Heap Sort.

---
+++ heapSort.py
@@ -0,0 +1,182 @@
+from os import system
+
+def heapifyOrdemAlfabetica(listaProdutos, tamanho, i):
+    raiz = i 
+    filhoEsquerda = (2*i) + 1
+    filhoDireita = (2*i) + 2
+
+    # confere existencia do filho da esquerda e se é maior que a raiz
+    if filhoEsquerda < tamanho and listaProdutos[raiz].nome < listaProdutos[filhoEsquerda].nome:
+        raiz = filhoEsquerda
+
+    # confere existencia do filho da direita e se é maior que a raiz
+    if filhoDireita < tamanho and listaProdutos[raiz].nome < listaProdutos[filhoDireita].nome:
+        raiz = filhoDireita
+    
+    if raiz != i:
+        #swap
+        aux = listaProdutos[i]
+        listaProdutos[i] = listaProdutos[raiz]
+        listaProdutos[raiz] = aux
+
+        #heapify para a nova raiz
+        heapifyOrdemAlfabetica(listaProdutos, tamanho, raiz)
+
+def heapifyDataLancamento(listaProdutos, tamanho, i):
+    raiz = i 
+    filhoEsquerda = (2*i) + 1
+    filhoDireita = (2*i) + 2
+
+    # confere existencia do filho da esquerda e se é maior que a raiz
+    if filhoEsquerda < tamanho and listaProdutos[raiz].dataLancamento > listaProdutos[filhoEsquerda].dataLancamento:
+        raiz = filhoEsquerda
+
+    # confere existencia do filho da direita e se é maior que a raiz
+    if filhoDireita < tamanho and listaProdutos[raiz].dataLancamento > listaProdutos[filhoDireita].dataLancamento:
+        raiz = filhoDireita
+    
+    if raiz != i:
+        #swap
+        aux = listaProdutos[i]
+        listaProdutos[i] = listaProdutos[raiz]
+        listaProdutos[raiz] = aux
+
+        #heapify para a nova raiz
+        heapifyDataLancamento(listaProdutos, tamanho, raiz)
+
+def heapifyPreco(listaProdutos, tamanho, i):
+    raiz = i 
+    filhoEsquerda = (2*i) + 1
+    filhoDireita = (2*i) + 2
+
+    # confere existencia do filho da esquerda e se é maior que a raiz
+    if filhoEsquerda < tamanho and listaProdutos[raiz].preco < listaProdutos[filhoEsquerda].preco:
+        raiz = filhoEsquerda
+
+    # confere existencia do filho da direita e se é maior que a raiz
+    if filhoDireita < tamanho and listaProdutos[raiz].preco < listaProdutos[filhoDireita].preco:
+        raiz = filhoDireita
+    
+    if raiz != i:
+        #swap
+        aux = listaProdutos[i]
+        listaProdutos[i] = listaProdutos[raiz]
+        listaProdutos[raiz] = aux
+
+        #heapify para a nova raiz
+        heapifyPreco(listaProdutos, tamanho, raiz)
+    
+def heapifyAvaliacao(listaProdutos, tamanho, i):
+    raiz = i 
+    filhoEsquerda = (2*i) + 1
+    filhoDireita = (2*i) + 2
+
+    # confere existencia do filho da esquerda e se é maior que a raiz
+    if filhoEsquerda < tamanho and listaProdutos[raiz].avaliacao > listaProdutos[filhoEsquerda].avaliacao:
+        raiz = filhoEsquerda
+
+    # confere existencia do filho da direita e se é maior que a raiz
+    if filhoDireita < tamanho and listaProdutos[raiz].avaliacao > listaProdutos[filhoDireita].avaliacao:
+        raiz = filhoDireita
+    
+    if raiz != i:
+        #swap
+        aux = listaProdutos[i]
+        listaProdutos[i]= listaProdutos[raiz]
+        listaProdutos[raiz] = aux
+
+        #heapify para a nova raiz
+        heapifyAvaliacao(listaProdutos, tamanho, raiz)
+
+def heapifyUnidadesVendidas(listaProdutos, tamanho, i):
+    raiz = i 
+    filhoEsquerda = (2*i) + 1
+    filhoDireita = (2*i) + 2
+
+    # confere existencia do filho da esquerda e se é maior que a raiz
+    if filhoEsquerda < tamanho and listaProdutos[raiz].numeroVendas > listaProdutos[filhoEsquerda].numeroVendas:
+        raiz = filhoEsquerda
+
+    # confere existencia do filho da direita e se é maior que a raiz
+    if filhoDireita < tamanho and listaProdutos[raiz].numeroVendas > listaProdutos[filhoDireita].numeroVendas:
+        raiz = filhoDireita
+    
+    if raiz != i:
+        #swap
+        aux = listaProdutos[i]
+        listaProdutos[i] = listaProdutos[raiz]
+        listaProdutos[raiz] = aux
+
+        #heapify para a nova raiz
+        heapifyUnidadesVendidas(listaProdutos, tamanho, raiz)
+
+def heapSort(listaProdutos, criterioOrdenacao): 
+    tamanho = len(listaProdutos)
+
+    if criterioOrdenacao == 1: #Max_Heap
+        # Constroi Heap
+        for i in range(tamanho, -1, -1): #Faz do final para o início do vetor
+            heapifyOrdemAlfabetica(listaProdutos, tamanho, i)
+
+        # Remove a Raiz
+        for i in range(tamanho-1, 0, -1):
+            aux = listaProdutos[i]
+            listaProdutos[i] = listaProdutos[0]
+            listaProdutos[0] = aux
+
+            heapifyOrdemAlfabetica(listaProdutos, i, 0)
+
+    elif criterioOrdenacao == 2: #Min_Heap
+        # Constroi Heap
+        for i in range(tamanho, -1, -1): #Faz do final para o início do vetor
+            heapifyDataLancamento(listaProdutos, tamanho, i)
+
+        # Remove a Raiz
+        for i in range(tamanho-1, 0, -1):
+            aux = listaProdutos[i]
+            listaProdutos[i] = listaProdutos[0]
+            listaProdutos[0] = aux
+
+            heapifyDataLancamento(listaProdutos, i, 0)
+
+    elif criterioOrdenacao == 3: #Max_Heap
+        # Constroi Heap
+        for i in range(tamanho, -1, -1): #Faz do final para o início do vetor
+            heapifyPreco(listaProdutos, tamanho, i)
+
+        # Remove a Raiz
+        for i in range(tamanho-1, 0, -1):
+            aux = listaProdutos[i]
+            listaProdutos[i] = listaProdutos[0]
+            listaProdutos[0] = aux
+
+            heapifyPreco(listaProdutos, i, 0)
+
+    elif criterioOrdenacao == 4: #Min_Heap
+        # Constroi Heap
+        for i in range(tamanho, -1, -1): #Faz do final para o início do vetor
+            heapifyAvaliacao(listaProdutos, tamanho, i)
+
+        # Remove a Raiz
+        for i in range(tamanho-1, 0, -1):
+            aux = listaProdutos[i]
+            listaProdutos[i] = listaProdutos[0]
+            listaProdutos[0] = aux
+
+            heapifyAvaliacao(listaProdutos, i, 0)
+
+    else: #Min_Heap
+        # Constroi Heap
+        for i in range(tamanho, -1, -1): #Faz do final para o início do vetor
+            heapifyUnidadesVendidas(listaProdutos, tamanho, i)
+
+        # Remove a Raiz
+        for i in range(tamanho-1, 0, -1):
+            aux = listaProdutos[i]
+            listaProdutos[i] = listaProdutos[0]
+            listaProdutos[0] = aux
+
+            heapifyUnidadesVendidas(listaProdutos, i, 0)
+    
+    return listaProdutos
+   
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from os import system
+import time
 from funcoes import *
+from heapSort import *
 
 def main():
     system('clear')
@@ -11,28 +13,55 @@ def main():
             nProdutos = escolheNumeroProdutos()
             print('Gerando registros...')
             listaProdutos = []
+            inicio = time.time()
             listaProdutos = criaProdutos(listaProdutos, nProdutos)
-            print('Tempo de execução: ')
+            fim = time.time()
+            print('Tempo de execução: {:.6f} s'.format (fim - inicio))
             input("\n\nAperte ENTER para continuar")
             print('====================== Lista de Produtos =========================\n')
             imprimeProdutos(listaProdutos)
-            input("\n\nAperte ENTER para continuar")
+            input("Aperte ENTER para continuar")
             while True:
                 criterioOrdenacao = exibeMenuOrdenacao()
                 if criterioOrdenacao == 1:
-                    print('Ordena por ordem alfabetica')
+                    print('----------------- Ordem Alfabética ---------------------------')
+                    inicioHeapOrdemAlfabetica = time.time()
+                    listaProdutos = heapSort(listaProdutos, criterioOrdenacao)
+                    fimHeapOrdemAlfabetica = time.time()
+                    imprimeProdutos(listaProdutos)
+                    print('Tempo de execução: {:.6f} s'.format (fimHeapOrdemAlfabetica - inicioHeapOrdemAlfabetica))
                     input("\n\nAperte ENTER para continuar")
                 elif criterioOrdenacao == 2:
-                    print('Ordena por data de lançamento')
+                    print('----------------- Mais Recentes ---------------------------')
+                    inicioHeapDataLancamento = time.time()
+                    listaProdutos = heapSort(listaProdutos, criterioOrdenacao)
+                    fimHeapDataLancamento = time.time()
+                    imprimeProdutos(listaProdutos)
+                    print('Tempo de execução: {:.6f} s'.format (fimHeapDataLancamento - inicioHeapDataLancamento))
                     input("\n\nAperte ENTER para continuar")
                 elif criterioOrdenacao == 3:
-                    print('Ordena por preço')
+                    print('----------------- Mais Baratos ---------------------------')
+                    inicioHeapPreco = time.time()
+                    listaProdutos = heapSort(listaProdutos, criterioOrdenacao)
+                    fimHeapPreco = time.time()
+                    imprimeProdutos(listaProdutos)
+                    print('Tempo de execução: {:.6f} s'.format (fimHeapPreco - inicioHeapPreco))
                     input("\n\nAperte ENTER para continuar")
                 elif criterioOrdenacao == 4:
-                    print('Ordena por avaliação')
+                    print('----------------- Melhores Avaliados ---------------------------')
+                    inicioHeapAvaliacao = time.time()
+                    listaProdutos = heapSort(listaProdutos, criterioOrdenacao)
+                    fimHeapAvaliacao = time.time()
+                    imprimeProdutos(listaProdutos)
+                    print('Tempo de execução: {:.6f} s'.format (fimHeapAvaliacao - inicioHeapAvaliacao))
                     input("\n\nAperte ENTER para continuar")
                 elif criterioOrdenacao == 5:
-                    print('Ordena por número de unidades vendidas')
+                    print('----------------- Mais Vendidos ---------------------------')
+                    inicioHeapUnidadesVendidas = time.time()
+                    listaProdutos = heapSort(listaProdutos, criterioOrdenacao)
+                    fimHeapUnidadesVendidas = time.time()
+                    imprimeProdutos(listaProdutos)
+                    print('Tempo de execução: {:.6f} s'.format (fimHeapUnidadesVendidas - inicioHeapUnidadesVendidas))
                     input("\n\nAperte ENTER para continuar")
                 else:
                     break
