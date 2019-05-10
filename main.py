666230fdbc9843a1bb264c0e997ebeee1671372d
LucasPenido <lpenidoa@me.com>
2019-05-10 18:22:09 -0300

    Adicionando ordenação com o Radix LSD.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from os import system
 from funcoes import *
+from radixSortLSD import *
+from time import clock
 
 def main():
     system('clear')
@@ -11,8 +13,12 @@ def main():
             nProdutos = escolheNumeroProdutos()
             print('Gerando registros...')
             listaProdutos = []
+            start_time = clock()
             listaProdutos = criaProdutos(listaProdutos, nProdutos)
+            end_time = clock()
             print('Tempo de execução: ')
+            elapsed_time = end_time - start_time
+            print(elapsed_time)
             input("\n\nAperte ENTER para continuar")
             print('====================== Lista de Produtos =========================\n')
             imprimeProdutos(listaProdutos)
@@ -21,19 +27,49 @@ def main():
                 criterioOrdenacao = exibeMenuOrdenacao()
                 if criterioOrdenacao == 1:
                     print('Ordena por ordem alfabetica')
+                    start_time = clock()
+                    radixSortLSDNomeProduto(listaProdutos)
+                    end_time = clock()
+                    print('Tempo total :')
+                    print(end_time - start_time)
                     input("\n\nAperte ENTER para continuar")
+                    imprimeProdutos(listaProdutos)
                 elif criterioOrdenacao == 2:
                     print('Ordena por data de lançamento')
+                    start_time = clock()
+                    radixSortLSDData(listaProdutos)
+                    end_time = clock()
+                    print('Tempo total :')
+                    print(end_time - start_time)
                     input("\n\nAperte ENTER para continuar")
+                    imprimeProdutos(listaProdutos)
                 elif criterioOrdenacao == 3:
                     print('Ordena por preço')
+                    start_time = clock()
+                    radixSortLSDPreco(listaProdutos)
+                    end_time = clock()
+                    print('Tempo total :')
+                    print(end_time - start_time)
                     input("\n\nAperte ENTER para continuar")
+                    imprimeProdutos(listaProdutos)
                 elif criterioOrdenacao == 4:
                     print('Ordena por avaliação')
+                    start_time = clock()
+                    radixSortLSDAvaliacao(listaProdutos)
+                    end_time = clock()
+                    print('Tempo total :')
+                    print(end_time - start_time)
                     input("\n\nAperte ENTER para continuar")
+                    imprimeProdutos(listaProdutos)
                 elif criterioOrdenacao == 5:
                     print('Ordena por número de unidades vendidas')
+                    start_time = clock()
+                    radixSortLSDNumeroUnidades(listaProdutos)
+                    end_time = clock()
+                    print('Tempo total :')
+                    print(end_time - start_time)
                     input("\n\nAperte ENTER para continuar")
+                    imprimeProdutos(listaProdutos)
                 else:
                     break
         else:
---
+++ radixSortLSD.py
@@ -0,0 +1,199 @@
+def _maiorNumeroPreco(vetor):
+    maiorNumero = 0
+    for i in vetor:
+        numero = i.preco
+        if numero > maiorNumero:
+            maiorNumero = numero
+
+    return maiorNumero
+
+def _countingSortPreco(vetor, exp):
+    tamVetor = len(vetor)
+    resultado = [0] * (tamVetor)
+    count = [0] * (10)
+
+    for i in range(0, tamVetor):
+        index = int((vetor[i].preco*100/exp)%10)
+        count[index] += 1
+
+    for i in range(1,10):
+        count[i] += count[i-1]
+
+    i = tamVetor-1
+    while i>=0:
+        index = int((vetor[i].preco*100/exp)%10)
+        resultado[count[index] - 1] = vetor[i]
+        count[index] -= 1
+        i -= 1
+
+    i = 0
+    for i in range(0,len(vetor)):
+        vetor[i] = resultado[i]
+
+def radixSortLSDPreco(vetor):
+    max = _maiorNumeroPreco(vetor)
+
+    exp = 1
+    while int(max*100/exp) > 0:
+        _countingSortPreco(vetor,exp)
+        exp *= 10
+
+def _countingSortData(vetor, digito):
+    tamVetor = len(vetor)
+    resultado = [0] * (tamVetor)
+    count = [0] * (123)
+
+    for i in range(0, tamVetor):
+        index = str(vetor[i].dataLancamento)[-digito]
+
+        count[ord(index)] += 1
+
+    for i in range(1,123):
+        count[i] += count[i-1]
+
+    i = tamVetor-1
+    while i>=0:
+        index = str(vetor[i].dataLancamento)[-digito]
+
+        resultado[count[ord(index)] - 1] = vetor[i]
+        count[ord(index)] -= 1
+        i -= 1
+
+    i = 0
+    for i in range(0,len(vetor)):
+        vetor[i] = resultado[i]
+
+def radixSortLSDData(vetor):
+
+    digitos = 10
+
+    for i in range(1, digitos+1):
+        _countingSortData(vetor,i)
+
+def _maiorNumeroAvaliacao(vetor):
+    maiorNumero = 0
+    for i in vetor:
+        numero = i.avaliacao
+        if numero > maiorNumero:
+            maiorNumero = numero
+
+    return maiorNumero
+
+def _countingSortAvaliacao(vetor, exp):
+    tamVetor = len(vetor)
+    resultado = [0] * (tamVetor)
+    count = [0] * (10)
+
+    for i in range(0, tamVetor):
+        index = int((vetor[i].avaliacao/exp)%10)
+        count[index] += 1
+
+    for i in range(1,10):
+        count[i] += count[i-1]
+
+    i = tamVetor-1
+    while i>=0:
+        index = int((vetor[i].avaliacao/exp)%10)
+        resultado[count[index] - 1] = vetor[i]
+        count[index] -= 1
+        i -= 1
+
+    i = 0
+    for i in range(0,len(vetor)):
+        vetor[i] = resultado[i]
+
+def radixSortLSDAvaliacao(vetor):
+    max = _maiorNumeroAvaliacao(vetor)
+
+    exp = 1
+    while int(max/exp) > 0:
+        _countingSortAvaliacao(vetor,exp)
+        exp *= 10
+
+def _maiorNumeroUnidades(vetor):
+    maiorNumero = 0
+    for i in vetor:
+        numero = i.numeroVendas
+        if numero > maiorNumero:
+            maiorNumero = numero
+
+    return maiorNumero
+
+def _countingSortNumeroUnidades(vetor, exp):
+    tamVetor = len(vetor)
+    resultado = [0] * (tamVetor)
+    count = [0] * (10)
+
+    for i in range(0, tamVetor):
+        index = int((vetor[i].numeroVendas/exp)%10)
+        count[index] += 1
+
+    for i in range(1,10):
+        count[i] += count[i-1]
+
+    i = tamVetor-1
+    while i>=0:
+        index = int((vetor[i].numeroVendas/exp)%10)
+        resultado[count[index] - 1] = vetor[i]
+        count[index] -= 1
+        i -= 1
+
+    i = 0
+    for i in range(0,len(vetor)):
+        vetor[i] = resultado[i]
+
+def radixSortLSDNumeroUnidades(vetor):
+    max = _maiorNumeroUnidades(vetor)
+
+    exp = 1
+    while int(max/exp) > 0:
+        _countingSortNumeroUnidades(vetor,exp)
+        exp *= 10
+
+def _maiorQntdDigitos(vetor):
+    maiorDigitos = 0
+    for i in vetor:
+        tam = len(i.nome)
+        if tam > maiorDigitos:
+            maiorDigitos = tam
+
+    return maiorDigitos
+
+def _countingSortNomeProduto(vetor, digito):
+    tamVetor = len(vetor)
+    resultado = [0] * (tamVetor)
+    count = [0] * (123)
+
+    for i in range(0, tamVetor):
+        # print(vetor[i])
+        # index = vetor[i][len(vetor[i]) - digito : len(vetor[i]) - (digito - 1)]
+        if digito <= len(vetor[i].nome):
+            index = vetor[i].nome[-digito]
+        else:
+            index = vetor[i].nome[0]
+        # print(ord(index))
+        count[ord(index)] += 1
+
+    for i in range(1,123):
+        count[i] += count[i-1]
+
+    i = tamVetor-1
+    while i>=0:
+        if digito <= len(vetor[i].nome):
+            index = vetor[i].nome[-digito]
+        else:
+            index = vetor[i].nome[0]
+        resultado[count[ord(index)] - 1] = vetor[i]
+        count[ord(index)] -= 1
+        i -= 1
+
+    i = 0
+    for i in range(0,len(vetor)):
+        vetor[i] = resultado[i]
+
+def radixSortLSDNomeProduto(vetor):
+
+    digitos = _maiorQntdDigitos(vetor)
+
+    for i in range(1, digitos+1):
+        _countingSortNomeProduto(vetor,i)
